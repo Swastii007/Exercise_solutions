231536dbca0cb9d320f6bd70ca3f6776e82a3390
Swastii007 <151751128+Swastii007@users.noreply.github.com>
2024-01-27 16:07:49 +0100

    Add files via upload

---
+++ Assignment_3B.R
@@ -0,0 +1,19 @@
+
+remind_me <- function() {
+  message("Don't forget to buy: Milk, Hummus, Bread")
+}
+
+cheat <- function(exercise_number) {
+ 
+  solutions <- list(
+    `Exercise 3.1.1` = "Solution 1"
+  )
+  
+  if (exercise_number %in% names(solutions)) {
+    message(paste("For", exercise_number, ":", solutions[[exercise_number]]))
+  } else {
+    warning("Exercise not found.")
+  }
+}
+
+#Link to the functions: https://github.com/SwastiSharma/your-repo/remind_me_cheat.R
